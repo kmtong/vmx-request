0423d1bce768ee325d2ca911fcec3f191caf0df8
KM Tong <kmtong@yunbaoguan.cn>
2021-03-17 16:22:15 +0800

    init checkin

---
+++ README.md
@@ -0,0 +1,64 @@
+# How to Use
+
+## Setup
+
+```js
+import VueModx from 'vue-modx'
+import RequestModule from 'vmx-request'
+import RequestElementUIModule from 'vmx-request-elm' // provide UI handler for HTTP requests
+
+Vue.use(VueModx, {
+    modules: [RequestModule, RequestElementUIModule],
+    config: {
+        http: {
+            endpoint: "http://localhost:8080/",
+            timeout: 5000 
+        }
+    }
+})
+
+```
+
+## Make a Request
+
+In Vue:
+
+```js
+async request() {
+    let resp = await this.$request({
+        url: `/path`,
+        method: 'get'
+    })
+    console.log(resp);
+},
+async requestUI() {
+    // with UI handler (loading/error)
+    let resp = await this.$requestUI({
+        url: `/path`,
+        method: 'get'
+    })
+    console.log(resp);
+}
+```
+
+Wrapping Request into API:
+
+* api.js
+```js
+async function getUser(request, userId) {
+    const response = await request({ url: '/user/info', method: 'get' })
+    return response.data
+}
+```
+
+* in Vue
+```
+    import api from 'api'
+
+    methods: {
+        async getUser() {
+            // pass-in this.$request as a request construction function
+            const user = await api.getUser(this.$request, this.userId)
+        }
+    }
+```
---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import axios from 'axios'
+
+const REQUEST_MODULE = "request";
+
+interface RequestUIHandler {
+    loading: (loading: Boolean) => void;
+    error: (err: any) => void;
+}
+
+type HeaderProvider = (path) => object;
+
+function createAxiosInstance(baseURL: string, timeout: number, headerInjectionFunc: HeaderProvider, uiHandler?: RequestUIHandler) {
+    const service = axios.create({
+        baseURL,
+        withCredentials: true, // send cookies when cross-domain requests
+        timeout // request timeout
+    })
+
+    // request interceptor
+    service.interceptors.request.use(
+        config => {
+            const addHeaders = headerInjectionFunc(config.url);
+            var headers;
+            if (config.headers) {
+                headers = { ...config.headers, ...addHeaders };
+            } else {
+                headers = addHeaders;
+            }
+            if (uiHandler) {
+                uiHandler.loading(true);
+            }
+            return { ...config, headers };
+        },
+        error => {
+            if (uiHandler) {
+                uiHandler.error(error);
+            }
+            if (uiHandler) {
+                uiHandler.loading(false);
+            }
+            return Promise.reject(error)
+        }
+    )
+
+    // response interceptor
+    service.interceptors.response.use(
+        response => {
+            if (uiHandler) {
+                uiHandler.loading(false);
+            }
+            return response;
+        },
+        error => {
+            if (uiHandler) {
+                uiHandler.error(error);
+            }
+            if (uiHandler) {
+                uiHandler.loading(false);
+            }
+            return Promise.reject(error)
+        }
+    )
+    return service;
+}
+
+export default {
+    name: REQUEST_MODULE,
+    dependsOn: null,
+    extensionPoints: {
+        "request.ui": ({ registry }, obj: RequestUIHandler) => {
+            registry.moduleVarSet(REQUEST_MODULE, "ui", obj);
+        },
+        // request header injection function registration
+        // the registered function will be called for every request for header provision
+        "request.header": ({ registry }, obj: HeaderProvider) => {
+            registry.moduleVarAppend(REQUEST_MODULE, "headerProvider", obj);
+        }
+    },
+    start({ vue, registry }) {
+
+        // create an axios instance
+        let httpCfg = registry.configGet('http');
+
+        let baseURL = httpCfg ? httpCfg['endpoint'] : null;
+        let timeout = httpCfg ? httpCfg['timeout'] : 15000;
+
+        const headerProviders = registry.moduleVarGet(REQUEST_MODULE, "headerProvider");
+        const flattenHeaderProviders = headerProviders ? headerProviders.reduce((a, b) => [...a, ...b], []) : [];
+
+        const headerInjectionFunc = flattenHeaderProviders.length > 0
+            ? (path) => {
+                const headers = flattenHeaderProviders //
+                    .map(hp => hp(path)) //
+                    .reduce((h1, h2) => { return { ...h1, ...h2 } }, {});
+                return headers;
+            }
+            : () => { return {} }
+
+        // headless request
+        vue.prototype.$request = createAxiosInstance(baseURL, timeout, headerInjectionFunc);
+
+        // ui request
+        let uiHandler = registry.moduleVarGet(REQUEST_MODULE, "ui");
+        vue.prototype.$requestUI = createAxiosInstance(baseURL, timeout, headerInjectionFunc, uiHandler);
+
+    }
+}
